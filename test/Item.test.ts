7cae580005026a00869dfd07c9094b4450095a07
andrevitor103 <andrevitor103@gmail.com>
2022-10-03 20:03:00 -0300

    feature/aula_3 - alterando estrutura das pastas

--- test/Item.test.ts
+++ test/Item.test.ts
@@ -1,6 +1,6 @@
-import Coupon from "../src/Coupon";
-import { Dimension } from "../src/Dimension";
-import Item from "../src/Item";
+import Coupon from "../src/model/entity/Coupon";
+import { Dimension } from "../src/model/entity/Dimension";
+import Item from "../src/model/entity/Item";
 
 test("Não deve criar um item com dimensões negativas", function () {
 	expect(() => new Item(1, "Guitarra", 1000, new Dimension(-10, 20, -4), 10)).toThrow(new Error('Item com dimensões inválidas'));
